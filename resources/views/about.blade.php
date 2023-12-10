7e2868defd15c95ac5802fc33de0a158387357ff
Nora-Lootfy <96319423+Nora-Lootfy@users.noreply.github.com>
2023-12-10 22:02:00 +0200

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.template')
+
+@section('content')
+    @include('includes.pageHeader')
+    @include('includes.about')
+    @include('includes.callToAction')
+    @include('includes.team')
+
+@endsection
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.template')
+
+@section('content')
+    @include('includes.carousel')
+    @include('includes.facilities')
+    @include('includes.about')
+    @include('includes.callToAction')
+    @include('includes.classes')
+    @include('includes.appointment')
+    @include('includes.team')
+    @include('includes.testimonial')
+@endsection
